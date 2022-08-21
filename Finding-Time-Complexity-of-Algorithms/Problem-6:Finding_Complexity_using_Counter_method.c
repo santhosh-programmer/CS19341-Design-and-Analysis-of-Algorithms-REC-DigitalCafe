b541ad29e56289ca9afc05930bd9534c78c91085
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-08-22 00:11:44 +0530

    Update Problem-6:Finding_Complexity_using_Counter_method.c

--- Finding-Time-Complexity-of-Algorithms/Problem-6:Finding_Complexity_using_Counter_method.c
+++ Finding-Time-Complexity-of-Algorithms/Problem-6:Finding_Complexity_using_Counter_method.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+void reverse(int n,int *c)
+{
+   int rev = 0, remainder;
+   (*c)+=2;
+   while (n != 0) 
+    {
+        (*c)+=1;
+        remainder = n % 10;
+        (*c)+=1;
+        rev = rev * 10 + remainder;
+        (*c)+=1;
+        n/= 10;
+        (*c)+=1;
+    }
+    (*c)+=1;
+    // print(rev);
+}
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    reverse(n,&c);
+    printf("%d",c);
+}
