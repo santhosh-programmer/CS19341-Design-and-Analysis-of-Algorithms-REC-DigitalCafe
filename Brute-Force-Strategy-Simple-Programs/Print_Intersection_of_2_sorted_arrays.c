f59378ca84437335b2aac289fea866d79563c6b6
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-08-29 00:30:41 +0530

    Update Print_Intersection_of_2_sorted_arrays.c

--- Brute-Force-Strategy-Simple-Programs/Print_Intersection_of_2_sorted_arrays.c
+++ Brute-Force-Strategy-Simple-Programs/Print_Intersection_of_2_sorted_arrays.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a,b;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++)
+    scanf("%d",&arr[i]);
+    scanf("%d",&b);
+    int brr[b];
+    for(int i=0;i<b;i++)
+    scanf("%d",&brr[i]);
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<b;j++)
+        {
+            if(arr[i]==brr[j])
+            {
+                printf("%d ",arr[i]);
+                break;                
+            }
+        }
+    }
+}
