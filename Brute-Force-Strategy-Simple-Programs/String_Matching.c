b52594064c44bcaf82418e827b94581588d0ec11
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-08-28 17:10:31 +0530

    Update String_Matching.c

--- Brute-Force-Strategy-Simple-Programs/String_Matching.c
+++ Brute-Force-Strategy-Simple-Programs/String_Matching.c
@@ -1,23 +1,3 @@
-// Given a string and a pattern identify whether the pattern occurs in the given string or not. If the pattern occurs in the given string then print 1 otherwise print 0.
-
-// Input Format:
-
-// First Line - String
-
-// Second Line - Pattern
-
-// Output Format:
-
-// 1 - If Pattern occurs in the given String
-
-// 0 - If Pattern doesn't occur in the given String
-
-// For example:
-
-//   Input	     Result
-// Rajalakshmi     1
-// lak
-
 #include<stdio.h>
 #include<string.h>
 int find(char *s,char *f)
@@ -38,6 +18,3 @@ int main()
     scanf("%s",f);
     find(s,f)?printf("1"):printf("0");
 }
-
-
-
