ea5af17adce86b4f8cf1478685ca1c38e81ed165
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-01 18:51:14 +0530

    Create 2.c

---
+++ Basic-Programming/Basic-01/2.c
@@ -0,0 +1,14 @@
+// Write a C program to find the sum first N natural numbers.
+
+// For example:
+
+// Input	Result
+// 3       6
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",n*(n+1)/2);
+}
