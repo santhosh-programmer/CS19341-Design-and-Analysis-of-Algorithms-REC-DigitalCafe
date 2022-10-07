1d1bb2cf4392f1f1d0c98545c05f10dce5054738
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-10-07 22:31:41 +0530

    Create 2.c

---
+++ Basic-Programming/BASIC/2.c
@@ -0,0 +1,9 @@
+// Write a C program to find the quotient and reminder of given integers. 
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n%d",a/b,a%b);
+}
