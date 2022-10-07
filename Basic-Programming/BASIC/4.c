cd8dd9cc3cba737dcd49d9871b52a1d56437498a
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-10-07 22:32:20 +0530

    Create 4.c

---
+++ Basic-Programming/BASIC/4.c
@@ -0,0 +1,9 @@
+// Write a C program to find whether the given integer is odd or even? 
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    (a%2==0)?printf("Even"):printf("Odd");
+}
