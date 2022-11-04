40dfd107c0454e4f91afcb3cd0f00ff57baa4c5b
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-11-04 18:49:09 +0530

    optimized the solution

--- Greedy-Algorithms/Greedy-PP6.c
+++ Greedy-Algorithms/Greedy-PP6.c
@@ -1,18 +1,6 @@
-// NOTE:
-
-// There is some error in Greeedy-PP6 Question.
-// According to the Question, my code is the correct code but one test case's answer is wrong in digital cafe.
-// Wrong Test case inputs are,
-	
-// 6
-// 1 10 14 14 14 15
-// 6
-
-// Actual Solution to this Test case is 5 (DAMN SURE) but in digital cafe it is given as 2 (Wrong)...That's why i hardcoded for that particular case alone.
-
 #include<stdio.h>
 #include<stdlib.h>
-int find(int a[],int s,int n,int m,int o)
+int find(int a[],int s,int n,int m)
 {
     int t1,t2;
     if(s==n)
@@ -28,11 +16,11 @@ int find(int a[],int s,int n,int m,int o)
         return abs(max-min);
     }
     a[s]+=m;
-    t1=find(a,s+1,n,m,1);
+    t1=find(a,s+1,n,m);
     a[s]-=m; 
     a[s]-=m;
     if(!(a[s]<0))
-    t2=find(a,s+1,n,m,0);
+    t2=find(a,s+1,n,m);
     else
     t2=t1;
     a[s]+=m;
@@ -40,23 +28,11 @@ int find(int a[],int s,int n,int m,int o)
 }
 int main()
 {
-    int n,m,t1,t2;
+    int n,m;
     scanf("%d",&n);
-    if(n==6)
-    printf("2");
-    else
-    {
-        int a[n];
-        for(int i=0;i<n;i++)
-        scanf("%d",&a[i]);
-        scanf("%d",&m);
-        a[0]+=m;
-        t1=find(a,1,n,m,1);
-        a[0]-=2*m;
-        if(!(a[0]<0))
-        t2=find(a,1,n,m,0);
-        else
-        t2=t1;
-        (t1<t2)?printf("%d",t1):printf("%d",t2);
-    }
+    int a[n];
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    scanf("%d",&m);
+    printf("%d",find(a,0,n,m));
 }
